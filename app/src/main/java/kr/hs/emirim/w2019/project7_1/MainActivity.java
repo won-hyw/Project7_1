185684d937f21788f335c38112ca9d7d7c8f23d7
won-hyw <w2019@e-mirim.hs.kr>
2021-07-22 09:21:17 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/w2019/project7_1/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.w2019.project7_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
