d4a7d96dd4622c4da0046057287a8733abedaedc
won-hyw <w2019@e-mirim.hs.kr>
2021-07-22 09:48:16 +0900

    Edit MainActivity.java(create option menu) Commit

--- app/src/main/java/kr/hs/emirim/w2019/project7_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project7_1/MainActivity.java
@@ -3,12 +3,29 @@ package kr.hs.emirim.w2019.project7_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear1;
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linear1 = findViewById(R.id.linear1);
+        btn1 = findViewById(R.id.btn1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return true;
     }
 }
