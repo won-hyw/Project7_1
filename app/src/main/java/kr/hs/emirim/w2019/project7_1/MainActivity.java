9f12a9a0d061b7f1a9b9a38841eac54edf72b492
won-hyw <w2019@e-mirim.hs.kr>
2021-07-22 10:10:24 +0900

    Edit MainActivity.java(onOptionsItemSelected) Commit

--- app/src/main/java/kr/hs/emirim/w2019/project7_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project7_1/MainActivity.java
@@ -1,10 +1,13 @@
 package kr.hs.emirim.w2019.project7_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -27,5 +30,27 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu1, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_red:
+                linear1.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.item_yellow:
+                linear1.setBackgroundColor(Color.YELLOW);
+                return true;
+            case R.id.item_blue:
+                linear1.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_rotate:
+                btn1.setRotation(45);
+                return true;
+            case R.id.item_zoom:
+                btn1.setScaleX(2);
+                return true;
+        }
+        return false;
     }
 }
